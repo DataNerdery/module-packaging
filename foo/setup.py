29eb32768268c519c05a55156e966857a930c54e
DataNerdery <20151953+DataNerdery@users.noreply.github.com>
2018-01-13 12:02:47 +1100

    Create setup.py

---
+++ foo/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='foobar',
+    version='0.1',
+    description='Example foobar module',
+    url='http://github.com/DataNerdery/module-packaging',
+    author='Data Nerdery',
+    author_email='20151953+DataNerdery@users.noreply.github.com',
+    license='MIT',
+    packages=['foobar'],
+    zip_safe=False)
